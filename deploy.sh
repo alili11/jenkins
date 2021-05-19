f35725077ef8bcd67fd692dbca32646ef31d62c4
alili11 <83454323+alili11@users.noreply.github.com>
2021-05-19 15:08:27 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-scp -i /secrets/kp-alison.pem helloWorld.class ec2-3-22-250-36.us-east-2.compute.amazonaws.com:/app/
-ssh -i /secrets/kp-alison.pem ec2-3-22-250-36.us-east-2.compute.amazonaws.com 'cd /app && java helloWorld'
+ssh -o StrictHostKeyChecking=no -i /secrets/kp-alison.pem ec2-3-22-250-36.us-east-2.compute.amazonaws.com 'sudo mkdir /app'
+scp StrictHostKeyChecking=no -i /secrets/kp-alison.pem helloWorld.class ec2-3-22-250-36.us-east-2.compute.amazonaws.com:/app/
+ssh StrictHostKeyChecking=no -i /secrets/kp-alison.pem ec2-3-22-250-36.us-east-2.compute.amazonaws.com 'cd /app && java helloWorld'
