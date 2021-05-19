fc8114212f2686cb3633e1ab2b5c350d0c4cb467
alili11 <83454323+alili11@users.noreply.github.com>
2021-05-19 15:02:56 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-sudo mkdir -p /app
-sudo cp helloWorld.class /app
-sudo cd /app
-java helloWorld
+scp -i /secrets/kp-alison.pem helloWorld.class ec2-3-22-250-36.us-east-2.compute.amazonaws.com:/app/
+ssh -i /secrets/kp-alison.pem ec2-3-22-250-36.us-east-2.compute.amazonaws.com 'cd /app && java helloWorld'
